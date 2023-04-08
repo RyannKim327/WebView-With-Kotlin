b8dbecf26e7d98512f54016f7ed26033bdbd208f
RyannKim327 <rksesgundo123@gmail.com>
2023-04-08 21:59:27 +0800

    04-08-23 21:59

--- app/src/main/java/a/a.kt
+++ app/src/main/java/a/a.kt
@@ -1,6 +1,7 @@
 package a
 
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -8,47 +9,60 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.LinearLayout.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 class a : AppCompatActivity(){
 	val DEFAULT_LINK = "https://www.google.com"
+
+	lateinit var web_normal: WebView
+	lateinit var web_incog: WebView
+
 	override fun onCreate(bundle: Bundle?){
 		super.onCreate(bundle)
 		val base = LinearLayout(this)
-		val tabs = LinearLayout(this)
 		val normal = Button(this)
-		val incognito = Button(this)
-		val web_normal = WebView(this)
-		val web_incog = WebView(this)
+		web_normal = WebView(this)
+		web_incog = WebView(this)
 
-		base.setOrientation(LinearLayout.VERTICAL)
-		tabs.setOrientation(LinearLayout.HORIZONTAL)
+		base.orientation = LinearLayout.VERTICAL
 
-		tabs.setLayoutParams(LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT))
+		normal.text = "Normal Mode"
 
-		normal.setText("Normal Mode")
-		incognito.setText("Incognito")
+		normal.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
 
-		normal.setLayoutParams(LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, 0.5f))
-		incognito.setLayoutParams(LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, 0.5f))
+		web_normal.visibility = View.VISIBLE;
+		web_incog.visibility = View.GONE;
 
-		tabs.addView(normal)
-		tabs.addView(incognito)
-
-		web_normal.setWebViewClient(WebViewClient())
-		web_normal.setWebChromeClient(WebChromeClient())
-		web_incog.setWebViewClient(WebViewClient())
-		web_incog.setWebChromeClient(WebChromeClient())
+		web_normal.webViewClient = WebViewClient()
+		web_normal.webChromeClient = WebChromeClient()
+		web_incog.webViewClient = WebViewClient()
+		web_incog.webChromeClient = WebChromeClient()
 
 		web_normal.loadUrl(DEFAULT_LINK)
 		web_incog.loadUrl(DEFAULT_LINK)
 
-		web_normal.setLayoutParams(LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT))
-		web_incog.setLayoutParams(LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT))
+		web_normal.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+		web_incog.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT)
 
-		base.addView(tabs)
+		base.addView(normal)
 		base.addView(web_normal)
 		base.addView(web_incog)
 
 		setContentView(base)
+	}
+	override fun onBackPressed() {
+		if(web_normal.visibility == View.VISIBLE && web_incog.visibility == View.GONE){
+			if(web_normal.canGoBack()){
+				web_normal.goBack()
+			}else{
+				super.onBackPressed()
+			}
+		}else if(web_incog.visibility == View.VISIBLE){
+			if(web_incog.canGoBack()){
+				web_incog.goBack()
+			}else{
+				super.onBackPressed()
+			}
+		}
 	}
 }
